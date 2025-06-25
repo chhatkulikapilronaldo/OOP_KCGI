db95b9cc1c2ff905a35f7f308b9a9b85a8223f5f
ACER <chhatkulikapil997@gmail.com>
2025-06-25 17:42:05 +0900

    collection of oop

---
+++ twentyfive/spring/oop/groupe1/lession8/Task.java
@@ -0,0 +1,11 @@
+package twentyfive.spring.oop.group1.lesson10.sample;
+
+public class Task {
+    private String author;
+    private String responsiblePerson;
+    private String deadline;
+    private String note;
+    private String name;
+    //constructor, getters, setters, toString() methods …
+}
+
